dfea599a630fea564373f6459951230f01691650
Carias <diegocarias789@gmail.com>
2025-01-30 20:42:37 -0600

    add: Rol CRUD

--- Models/RolModel.cs
+++ Models/RolModel.cs
@@ -10,8 +10,12 @@ namespace ControlEdificioF.Models
     {
         private int _RolID;
         private string _Rol;
+        private int _EstadoId;
+        private string _Estado;
 
         public int RolID { get => _RolID; set => _RolID = value; }
         public string Rol { get => _Rol; set => _Rol = value; }
+        public int EstadoId { get => _EstadoId; set => _EstadoId = value; }
+        public string Estado { get => _Estado; set => _Estado = value; }
     }
 }
