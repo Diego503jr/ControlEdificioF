61b84513fc4e076ff8f40c4a712b8fa3761e819e
Carias <diegocarias789@gmail.com>
2025-01-30 20:46:04 -0600

    add: documentation's database

---
+++ Services/Database/ConfigDb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ControlEdificioF.Services.Database
+{
+        /// <summary>
+        /// Implementación de IConfigDb que maneja los parámetros de conexión a base de datos
+        /// </summary>
+    internal class ConfigDb : IConfigDb
+    {
+        // Campos privados para almacenar credenciales
+        private string _server;
+        private string _db;
+        private string _user;
+        private string _pwd;
+    
+        // Las propiedades públicas son auto-explicativas por sus nombres
+        public string Server { get => _server; set => _server = value; }
+        public string Db { get => _db; set => _db = value; }
+        public string User { get => _user; set => _user = value; }
+        public string Pwd { get => _pwd; set => _pwd = value; }
+    
+        /// <summary>
+        /// Genera la cadena de conexión en formato estándar para SQL Server
+        /// usando los parámetros almacenados
+        /// </summary>
+        /// <returns>Cadena de conexión formateada</returns>
+        public string GetConnectionString()
+        {
+            return $"Server={Server};Database={Db};UId={User};Password={Pwd}";
+        }
+    }
+}
---
+++ Services/Database/DbContextMySql.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlEdificioF.Services.Database
+{
+    /// <summary>
+    /// Clase base abstracta que proporciona funcionalidad básica para conexiones MySQL
+    /// </summary>
+    internal abstract class DbContextMySql : IDbContextMySql
+    {
+        // Configuración de base de datos inyectada a través del constructor
+        protected readonly ConfigDb _configDb;
+
+        /// <summary>
+        /// Constructor que recibe la configuración necesaria para establecer conexiones
+        /// </summary>
+        protected DbContextMySql(ConfigDb configDb)
+        {
+            _configDb = configDb;
+        }
+
+        /// <summary>
+        /// Crea y abre una nueva conexión MySQL usando la configuración proporcionada
+        /// </summary>
+        /// <returns>Conexión MySQL activa y abierta</returns>
+        public MySqlConnection CreateConnection()
+        {
+            var connection = new MySqlConnection(_configDb.GetConnectionString());
+            connection.Open();
+            return connection;
+        }
+
+        /// <summary>
+        /// Cierra una conexión MySQL de forma segura si está abierta
+        /// </summary>
+        /// <param name="connection">Conexión a cerrar</param>
+        public void CloseConnection()
+        {
+            var connection = new MySqlConnection(_configDb.GetConnectionString());
+            if (connection.State != ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
