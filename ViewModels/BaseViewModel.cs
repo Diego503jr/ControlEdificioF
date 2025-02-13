cb00db01428631093013223cdf7c271192aed618
Carias <diegocarias789@gmail.com>
2025-02-12 19:56:07 -0600

    add: roots of viewmodels

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlEdificioF.ViewModels
+{
+    /// <summary>
+    /// Clase base que implementa notificación de cambios de propiedades para patrones MVVM
+    /// </summary>
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Evento que notifica cuando una propiedad cambia su valor
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Método protegido que dispara la notificación de cambio para una propiedad específica
+        /// </summary>
+        /// <param name="propertyname">Nombre de la propiedad que ha cambiado</param>
+        protected virtual void OnPropertyChanged(string propertyname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+    }
+}
---
+++ ViewModels/RolViewModel.cs
@@ -0,0 +1,75 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using ControlEdificioF.Models;
+using ControlEdificioF.Services.Contexts;
+using ControlEdificioF.Services.Database;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ControlEdificioF.ViewModels
+{
+    internal class RolViewModel : BaseViewModel
+    {
+        private readonly ConfigDb _config;
+        private ObservableCollection<RolModel> _roles;
+        private RolModel _rol;
+        private RolContext _rolcontext;
+
+        public RolViewModel()
+        {
+            _config = new ConfigDb()
+            {
+                Server = ConfigurationManager.AppSettings["SERVER"],
+                Db = ConfigurationManager.AppSettings["DATABASE"],
+                User = ConfigurationManager.AppSettings["USER"],
+                Pwd = ConfigurationManager.AppSettings["PASSWORD"]
+            };
+            _rol = new RolModel();
+            _roles = _rolcontext.Read();
+        }
+
+        public RolModel Rol
+        {
+            get => _rol;
+            set
+            {
+                _rol = value;
+                OnPropertyChanged(nameof(Rol));
+            }
+        }
+
+        public ObservableCollection<RolModel> Roles
+        {
+            get => _roles;
+            set
+            {
+                _roles = value;
+                OnPropertyChanged(nameof(Roles));
+            }
+        }
+
+        public ICommand RolCreateCommand
+        {
+            get
+            {
+                return new RelayCommand(RolExecuteCreate, RolCanExecuteCreate);
+            }
+        }
+
+        private void RolExecuteCreate()
+        {
+            _rolcontext.Create(Rol);
+            Roles = _rolcontext.Read();
+        }
+
+        private bool RolCanExecuteCreate()
+        {
+            return true;
+        }
+    }
+}
